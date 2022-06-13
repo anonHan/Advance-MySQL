9c33abc6a6ce991c5e1dda44892456782b317e15
Sahil Sharma <35657846+anonHan@users.noreply.github.com>
2022-06-13 23:37:49 +0530

    Delete 1. Analyzing the Traffic Sources directory

--- 1. Analyzing the Traffic Sources/1. Business Probem.sql
+++
@@ -1,1 +0,0 @@
-https://www.google.com/search?q=business&rlz=1C1RXQR_enIN973IN973&hl=en&tbm=isch&sxsrf=ALiCzsbEk7fHSnkjosmevZUc-Bu_Bw0Ukw:1655143149942&source=lnms&sa=X&ved=2ahUKEwj9vITcgKv4AhUW63MBHQPfB6UQ_AUoA3oECBQQBQ&biw=1745&bih=881&dpr=1.1#imgrc=FeSGsbdkSKptmM
